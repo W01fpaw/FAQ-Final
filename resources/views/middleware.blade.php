6c1357825a9ce3fb27ede933a292bb010a9669eb
W01fpaw <wsarchett@comcast.net>
2018-05-03 15:16:53 -0700

    Added view for unauthorized and middleware testing.

---
+++ resources/views/middleware.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="title m-b-md">
+
+        {{ strtoupper ($message) }} only page!
+
+    </div>
+
+@endsection
---
+++ resources/views/unauthorized.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="title m-b-md">
+
+        You cannot access this page! This is for only '{{$role}}!
+        
+    </div>
+
+@endsection
